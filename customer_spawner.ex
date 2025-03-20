bbcc9d1666cc9557a44034635fc1249cd38ae498
Maria Thomas <mlthoma@unc.edu>
2025-03-20 17:56:17 -0400

    delete all stuff

--- customer_spawner.ex
+++
@@ -1,5 +0,0 @@
-defmodule CustomerSpawner do
-  def spawn_customer(wait_time) do
-    Customer.start_link(wait_time)
-  end
-end
--- receptionist.ex
+++
@@ -1,8 +0,0 @@
-defmodule Receptionist do
-  def greet_and_direct(customer) do
-    case WaitingRoom.add_customer(customer) do
-      :ok -> IO.puts("Receptionist: Customer added to the waiting room.")
-      :full -> IO.puts("Receptionist: No space, customer turned away.")
-    end
-  end
-end
