df140f28cb75c70dcb2f90ca8380b4b58d6453d9
Maria Thomas <111470032+mlthoma@users.noreply.github.com>
2025-03-23 12:39:15 -0400

    Create customer_behavior_impatient.ex

---
+++ customer_behavior_impatient.ex
@@ -0,0 +1,16 @@
+defmodule CustomerBehavior do
+  def wait_for_haircut(barber, id) do
+    wait_time_limit = :rand.uniform(5000) + 5000 # Max 10s of wait
+
+
+    receive do
+      :haircut_done ->
+        IO.puts("Customer #{id} got haircut and leaves happy")
+        exit(:normal)
+    after
+      wait_time_limit ->
+        IO.puts("Customer #{id} got frustrated and leaves after waiting too long")
+        exit(:normal)
+    end
+  end
+end
